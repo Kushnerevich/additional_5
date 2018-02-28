5f95f2d9ee7d0cf66742759fffe187dfa99cd4e4
pavel <pasha2701@tut.by>
2018-02-28 12:32:51 +0300

    Did task 6

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+     pair = [];
+    for ( i = 0; i < bracketsConfig.length; i++) {
+        pair.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+    };
+
+    for ( z = 0; z < pair.length + 1; z++) {
+
+        for ( i = 0; i < pair.length; i++) {
+            for ( b = 0; b < str.length; b++) {
+                if (str.indexOf(pair[i]) != -1) {
+                    same = str.indexOf(pair[i]);
+                    str = str.substring(0, same) + str.substring(same + 2);
+                    b--;
+                };
+            };
+        };
+
+    };
+
+    if (str.length == 0) {
+        return (true);
+    }
+    else {
+        return (false);
+    };
+};
